b751756234bdbbf6233e8da6defd8aabec64a26a
Peter399c1 <cph-pm260@cphbusiness.dk>
2025-09-24 09:26:08 +0200

    main start

---
+++ codeLab5Sem3/src/main/java/app/daos/IDAO.java
@@ -0,0 +1,13 @@
+package app.daos;
+
+import java.util.List;
+
+public interface IDAO<T, I> {
+
+     T create(T t);
+
+     List<T> getAll();
+     T getById(I id);
+     T update(T t);
+     boolean delete(I id);
+}
